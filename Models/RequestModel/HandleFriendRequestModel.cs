e08d4db0f48a1705dadb9e3dff487e4a53d1de45
nmk2903 <nguyenminhkhoi02092003@gmail.com>
2023-11-16 11:41:43 +0700

    fix ne

--- Models/RequestModel/HandleFriendRequestModel.cs
+++ Models/RequestModel/HandleFriendRequestModel.cs
@@ -3,7 +3,7 @@
     public class HandleFriendRequestModel
     {
         public Guid friendId { get; set; }
-        public FriendModel.FriendStatus status { get; set; }
+        public FriendModel.FriendStatus? status { get; set; }
 
     }
 }
