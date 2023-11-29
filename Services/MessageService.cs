2ba32244863233196f1624cf049d92ece050a41a
nmk2903 <nguyenminhkhoi02092003@gmail.com>
2023-11-29 15:47:50 +0700

    fix

--- Services/MessageService.cs
+++ Services/MessageService.cs
@@ -22,8 +22,8 @@ namespace ChatChit.Services
             var messages = await _context.Messages
                  .Where(m => (m.senderId == currentUserId && m.receiverId == friendId) ||
                    (m.senderId == friendId && m.receiverId == currentUserId))
-                .OrderByDescending(m => m.createAt)
-                .Take(5)
+                .OrderBy(m => m.createAt)
+                .TakeLast(5)
                  .ToListAsync();
             var messageResponseList = new List<MessageResponseModel>();
 
